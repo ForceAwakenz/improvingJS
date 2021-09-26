25b6951d306ba1e4f50ea0ed5cb83cdd403b9fed
ForceAwakenz <83182111+ForceAwakenz@users.noreply.github.com>
2021-09-26 03:14:29 -0700

    add folder, write task for dom studying

--- DOM_events/script.js
+++ DOM_events/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+/*
+1. Play around with bubbling and capturing
+    a. stop capturing
+    b. stop bubbling
+    c. separate properties by stopping propagation for one but not another
+2. Try Event Delegation
+3. Implement smooth scrolling
+*/
