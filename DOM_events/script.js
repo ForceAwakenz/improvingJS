4d723ee41736f55256fa402cc93f36b73982ef44
ForceAwakenz <83182111+ForceAwakenz@users.noreply.github.com>
2021-09-26 06:00:04 -0700

    add 1 codewars uncompleted solve

--- DOM_events/script.js
+++ DOM_events/script.js
@@ -6,5 +6,6 @@
     b. stop bubbling
     c. separate properties by stopping propagation for one but not another
 2. Try Event Delegation
+    a. figure out where we pass this and where we pass event
 3. Implement smooth scrolling
 */
--- _codewars_checkout/script.js
+++ _codewars_checkout/script.js
@@ -1,2 +1,32 @@
 'use strict';
 
+// Rock Paper Scissors Oracle
+// https://www.codewars.com/kata/580535462e7b330bd300003d/train/javascript
+
+function oracle(gestures){
+
+    const gestureTotal = gestures.reduce((gest, total) => {
+        return total[gest]++
+    }, {rock: 0, paper: 0, scissors: 0});
+
+    const gestureWon = {};
+    gestureWon.rock = gestureTotal.scissors - gestureTotal.paper;
+    gestureWon.paper= gestureTotal.rock - gestureTotal.scissors;
+    gestureWon.scissors = gestureTotal.paper - gestureTotal.rock;
+
+    if (Object.values(gestureWon).reduce((won, acc) => won + acc, 0)) {
+        return 'tie';
+    } else {
+        for (let gest in gestureWon) {
+            if (gestureWon[gest] < 1) {
+                delete gestureWon[gest];
+            }
+        }
+        const resultArr = [];
+        Object.keys(gestureWon).forEach( g => {
+            resultArr.push(g);
+        });
+
+        return resultArr.join('/');
+    }
+  }
