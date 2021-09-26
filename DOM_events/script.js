854592b05b967cf2d44533328e9ab0ad4898c368
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-09-26 21:21:03 +0300

    complete last survivor kata

--- DOM_events/script.js
+++ DOM_events/script.js
@@ -9,3 +9,13 @@
     a. figure out where we pass this and where we pass event
 3. Implement smooth scrolling
 */
+
+const h1 = document.querySelector('h1');
+
+
+
+h1.addEventListener('click', function (e) {
+    console.log(e); // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
+    console.log(this); // <h1>DOM events</h1>
+    // ^ window object, if use arrow function
+});
--- _codewars_checkout/script.js
+++ _codewars_checkout/script.js
@@ -3,65 +3,74 @@
 // Rock Paper Scissors Oracle
 // https://www.codewars.com/kata/580535462e7b330bd300003d/train/javascript
 
-function oracle(gestures){
 
-    const gestureTotal = {rock: 0, paper: 0, scissors: 0};
-    gestures.forEach(gest => gestureTotal[gest]++);
+// function oracle(gestures){
 
-    console.log(gestureTotal);
+//     const gestureTotal = {rock: 0, paper: 0, scissors: 0};
+//     gestures.forEach(gest => gestureTotal[gest]++);
 
-    const gestureWon = {};
-    gestureWon.rock = gestureTotal.scissors - gestureTotal.paper;
-    gestureWon.paper= gestureTotal.rock - gestureTotal.scissors;
-    gestureWon.scissors = gestureTotal.paper - gestureTotal.rock;
+//     const gestureWon = {};
+//     gestureWon.rock = gestureTotal.scissors - gestureTotal.paper;
+//     gestureWon.paper= gestureTotal.rock - gestureTotal.scissors;
+//     gestureWon.scissors = gestureTotal.paper - gestureTotal.rock;
 
-    console.log(gestureWon);
+//     if (Object.values(gestureWon).every(gest => gest === 0)) {
+//         return 'tie';
+//     } else {
+//         const resultArr = [];
+//         for (let gest in gestureWon) {
+//             if (gestureWon[gest] > 0) {
+//                 resultArr.push(gest);
+//             }
+//         }
 
-    if (Object.values(gestureWon).every(gest => gest === 0)) {
-        return 'tie';
-    } else {
-        const resultArr = [];
-        for (let gest in gestureWon) {
-            if (gestureWon[gest] > 0) {
-                resultArr.push(gest);
-            }
-        }
-        
-        console.log(resultArr.join('/'));
-
-        return resultArr.join('/');
-    }
-  }
-
-  oracle(["rock","paper","scissors","rock"]);
-  oracle(["paper","scissors","scissors"]);
-  oracle(["paper","scissors","rock"]);
-
-// ========== Interesting solves:
-
-//   function oracle(gestures) {
-//     let result = [];
-    
-//     let obj = {
-//       "rock": 0,
-//       "paper": 0,
-//       "scissors": 0
+//         return resultArr.join('/');
 //     }
-    
-//     for (let i = 0; i < gestures.length; i++) {
-//       let gesture = gestures[i];
-//       obj[gesture]++;
-//     }
-    
-//     if (obj.scissors > obj.paper) result.push("rock"); // !
-//     if (obj.rock > obj.scissors) result.push("paper");
-//     if (obj.paper > obj.rock) result.push("scissors");
-    
-//     return result.join("/") || "tie"; // !
 //   }
+
+//   oracle(["rock","paper","scissors","rock"]);
+//   oracle(["paper","scissors","scissors"]);
+//   oracle(["paper","scissors","rock"]);
+
+// // ========== Interesting solves:
+
+// //   function oracle(gestures) {
+// //     let result = [];
+    
+// //     let obj = {
+// //       "rock": 0,
+// //       "paper": 0,
+// //       "scissors": 0
+// //     }
+    
+// //     for (let i = 0; i < gestures.length; i++) {
+// //       let gesture = gestures[i];
+// //       obj[gesture]++;
+// //     }
+    
+// //     if (obj.scissors > obj.paper) result.push("rock"); // !
+// //     if (obj.rock > obj.scissors) result.push("paper");
+// //     if (obj.paper > obj.rock) result.push("scissors");
+    
+// //     return result.join("/") || "tie"; // !
+// //   }
 
 // ========================================================================== 
 // ==========================================================================
 
+// Last Survivor
+// https://www.codewars.com/kata/609eee71109f860006c377d1/train/javascript
 
+
+function lastSurvivor(letters, coords) {
+
+    if (letters.length === 1) return letters.toString();
+    [...letters] = letters;
+    letters.splice(coords[0], 1);
+    coords.shift();
+    return lastSurvivor(letters, coords);
+
+}
+
+// console.log(lastSurvivor('kbc', [0, 1]));
 
