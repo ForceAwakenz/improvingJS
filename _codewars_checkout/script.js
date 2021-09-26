87d049ab181d4c24afee2d7098cb1ab303958521
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-09-26 22:26:23 +0300

    partly add plan to solve last survivor ep2

--- _codewars_checkout/script.js
+++ _codewars_checkout/script.js
@@ -62,15 +62,82 @@
 // https://www.codewars.com/kata/609eee71109f860006c377d1/train/javascript
 
 
-function lastSurvivor(letters, coords) {
+// function lastSurvivor(letters, coords) {
+    
+//     if (letters.length === 1) return letters.toString();
+//     [...letters] = letters;
+//     letters.splice(coords[0], 1);
+//     coords.shift();
+//     return lastSurvivor(letters, coords);
 
-    if (letters.length === 1) return letters.toString();
-    [...letters] = letters;
-    letters.splice(coords[0], 1);
-    coords.shift();
-    return lastSurvivor(letters, coords);
+// }
+
+// console.log(lastSurvivor('kbc', [0, 1]));
+
+// // ========== Interesting solves:
+
+// function lastSurvivor(letters, coords) {
+    //     letters = letters.split('');
+    //     for (let i = 0; i < coords.length; i++) {
+//        letters.splice(coords[i], 1)
+//     }
+//     return letters.join('')
+//  }
+
+// // ==========
+
+// function lastSurvivor(string, indices) {
+    //     const arr = [...string];
+    //     for (const i of indices) arr.splice(i, 1)
+    //     return arr[0];
+    //   }
+    
+    // // ==========
+    
+    // function lastSurvivor(letters, coords) {
+        //     const arr = [...letters]
+        //     coords.map((el, i, ar) => arr.splice(el, 1) )
+//     return arr[0]
+// }
+
+// // ==========
+
+// function lastSurvivor(letters, coords) {
+    //     for (let c of coords)
+    //       letters = letters.substr(0, c) + letters.substr(c+1);
+    //     return letters;
+    // }
+
+    
+    // // ==========
+
+    // function lastSurvivor(letters,coords) { 
+    //     return coords.reduce( (letters,coord) => letters.slice(0,coord) + letters.slice(coord+1) , letters ); 
+    // }
+    
+// // ==========
+
+// ========================================================================== 
+// ==========================================================================
+
+
+// Last Survivor Ep.2
+// https://www.codewars.com/kata/60a1aac7d5a5fc0046c89651/train/javascript
+
+function lastSurvivors(str) {
 
 }
 
-// console.log(lastSurvivor('kbc', [0, 1]));
+/*
+'azzzb'
+
+0. set counter to 0
+1. take first letter
+2. check if there is matches
+3. remove match1 and match2
+4. add match+1 letter to the end of the string
+5. if match letter is z - return a
+
+
+*/
 
