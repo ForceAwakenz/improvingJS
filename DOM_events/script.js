e7c37a71cea724cbe4f35b5f01e2c8884f081b51
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-09-27 15:43:43 +0300

    complete type coercion topic

--- DOM_events/script.js
+++ DOM_events/script.js
@@ -2,20 +2,48 @@
 
 /*
 1. Play around with bubbling and capturing
-    a. stop capturing
+    a. stop capturing 
     b. stop bubbling
     c. separate properties by stopping propagation for one but not another
 2. Try Event Delegation
-    a. figure out where we pass this and where we pass event
+    a. figure out where we pass this and where we pass event ✔
 3. Implement smooth scrolling
 */
 
 const h1 = document.querySelector('h1');
 
 
-
 h1.addEventListener('click', function (e) {
     console.log(e); // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
     console.log(this); // <h1>DOM events</h1>
     // ^ window object, if use arrow function
-});
+}, {once: true, capture: true}); // options
+
+// How 'this' works in higher order functions
+
+const obj = {
+    a: 10,
+    wrapper : function (func) {
+        console.log(this);// {a: 10, wrapper: ƒ}
+        func();
+    },
+}
+
+function thisIndicator(a) {
+    console.log(this); // undefined
+    // ========================= Why it works different then eventListener ?????????? ========================
+}
+
+obj.wrapper(thisIndicator);
+
+// ============================================
+
+const outer = document.getElementById('outerdiv');
+const middle = document.getElementById('midlediv');
+const inner1 = document.getElementById('inner1');
+const inner2 = document.getElementById('inner2');
+const inner3 = document.getElementById('inner3');
+
+
+outer.addEventListener('click', () => console.log('outer capturing'), true);
+outer.addEventListener('click', () => console.log('outer bubbling'));
